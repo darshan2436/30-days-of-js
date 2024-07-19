916cc564687305bd0700a4f140dee084418c8f91
darshan2436 <thapadarshan447@gmail.com>
2024-07-20 01:31:06 +0545

    js for OTPVerification

---
+++ OTPVerification.js
@@ -0,0 +1,37 @@
+const inputs = document.querySelectorAll("input"),
+buttons = document.querySelector("button");
+
+inputs.forEach((input, index1) => {
+    input.addEventListener("keyup", (e) => {
+        const currentInput = input,
+            nextInput = input.nextElementSibling,
+            previousInput = input.previousElementSibling;
+
+        if(currentInput.value.length > 1){
+            currentInput.value = "";
+            return;
+        }
+        
+        if(nextInput && nextInput.hasAttribute("disabled") && currentInput.value !== ""){
+            nextInput.removeAttribute("disabled");
+            nextInput.focus();
+        }
+    
+        if(e.key === "Backspace"){
+            inputs.forEach((input , index2) =>{
+                if(index1 <= index2 && previousInput){
+                    input.setAttribute("disabled", true);
+                    currentInput.value = "";
+                    previousInput.focus();
+                }
+            })
+        }
+
+        if(inputs[3].value !== ""){
+            buttons.removeAttribute("disabled");
+        }
+
+    })
+})
+
+window.addEventListener("load", () => inputs[0].focus())
