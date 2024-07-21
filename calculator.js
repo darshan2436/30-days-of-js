050ded39800a98d04433fd27f6cfbcd957943e1e
darshan2436 <thapadarshan447@gmail.com>
2024-07-21 22:57:39 +0545

    js for calculator

---
+++ calculator.js
@@ -0,0 +1,7 @@
+let screen = document.querySelector(".calculator-screen #screen");
+function getValueClick(e){
+    console.log(e);
+    // const value = document
+}
+document.addEventListener("keydown",getValueKey);
+document.addEventListener("click",getValueClick);
