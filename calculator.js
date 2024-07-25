73c31fce3cea0c02ba9a4f09295dc3a163ce7ace
darshan2436 <thapadarshan447@gmail.com>
2024-07-25 14:42:05 +0545

    handling error while the eval function get wrong input

--- calculator.js
+++ calculator.js
@@ -1,8 +1,20 @@
 let inputValue = document.getElementById("display");
+let acButton = document.getElementById("ac");
 function deleteValue(){
     let value = inputValue.value;
     inputValue.value = value.slice(0,-1);
 }
 function getValue(value){
     inputValue.value += value;
+}
+function calculate(){
+    try {
+        inputValue.value = parseFloat(eval(inputValue.value));
+    } catch (err) {
+        inputValue.value = `${err}`;
+        inputValue.style = "font-size:16px;";
+    }
+    acButton.addEventListener("click",()=>{
+        inputValue.style = "font-size:40px;";
+    })
 }
