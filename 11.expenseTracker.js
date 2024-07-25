c4c4b5a405c7db300023bf9b243f5ba207a426a5
darshan2436 <thapadarshan447@gmail.com>
2024-07-25 23:34:26 +0545

    finished expensed tracker(for today)

--- 11.expenseTracker.js
+++ 11.expenseTracker.js
@@ -1,12 +1,54 @@
-const expense = document.getElementById("expense");
-const expenseAmount = document.getElementById("amount");
-const expenseCategory = document.getElementById("category");
-const expenseDate = document.getElementById("date");
-const addBtn = document.getElementById("add-expense");
-const expenseConainer = document.getElementById("expense-table");
+document.addEventListener("DOMContentLoaded",()=>{
+    const addBtn = document.getElementById("add-expense");
+    const expenseContainer = document.getElementById("expense-table");
+    const totalExpense = document.getElementById("total");
+    const errorContainer = document.querySelector("#error-container p");
+    let expenseArr = [];
 
-function addValue(){
-    if()
-}
+    addBtn.addEventListener("click", ()=>{
+        const expenseName = document.getElementById("expense").value;
+        const expenseAmount = parseFloat(document.getElementById("amount").value);
+        const expenseCategory = document.getElementById("category").value;
+        const expenseDate = document.getElementById("date").value;
+        const expense = {
+            id: Date.now(),
+            expenseName,
+            expenseAmount,
+            expenseCategory,
+            expenseDate
+        };
+        if(checkInput(expense)){
+            expenseArr.push(expense);
+            addValue(expenseArr);
+            updateTotal();
+            return;
+        }
+    });
+    
+    function addValue(expenseArr){
+        expenseContainer.innerHTML = "";
+        expenseArr.forEach((expense) => {
+            expenseContainer.innerHTML += 
+            `<tr id="${expense.id}">
+                <td>${expense.expenseName}</td>
+                <td>Rs. ${expense.expenseAmount.toFixed(2)}</td>
+                <td>${expense.expenseCategory}</td>
+                <td>${expense.expenseDate}</td>
+            </tr> `
+        });
+    }
 
-addBtn.addEventListener("click",());
+    function updateTotal(){
+        const total = expenseArr.reduce((sum,expense)=> sum + expense.expenseAmount,0);
+        totalExpense.innerText = `${total}`;
+    }
+
+    function checkInput(expenseArr){
+        if(!expenseArr.expenseName || !expenseArr.expenseAmount || !expenseArr.expenseDate){
+            errorContainer.textContent = "*Please fill all the input fields";
+            return false;
+        }
+        errorContainer.textContent = "";
+        return true;
+    }
+});
