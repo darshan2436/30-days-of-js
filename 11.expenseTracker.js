fd213f1c620afef0f6baa4b9d554168645143be4
darshan2436 <thapadarshan447@gmail.com>
2024-07-25 21:53:36 +0545

    initial for expense Tracker

---
+++ 11.expenseTracker.js
@@ -0,0 +1,12 @@
+const expense = document.getElementById("expense");
+const expenseAmount = document.getElementById("amount");
+const expenseCategory = document.getElementById("category");
+const expenseDate = document.getElementById("date");
+const addBtn = document.getElementById("add-expense");
+const expenseConainer = document.getElementById("expense-table");
+
+function addValue(){
+    if()
+}
+
+addBtn.addEventListener("click",());
