a3df9ddea1ab6ece487e124b5717675ae132aed6
darshan2436 <thapadarshan447@gmail.com>
2024-08-19 16:14:17 +0545

    made the api connection secured

--- 29.WeatherApp.js
+++ 29.WeatherApp.js
@@ -12,7 +12,7 @@ formEl.addEventListener("submit",(e)=>{
 
 async function getData(city){
     try {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
     if(!response.ok){
         throw new Error("Network Response Was Not Ok");
     }
